58b6667c784a3a27231d2e42a240cb3ae26cdf86
alexisTobar <tobaralexis.89@gmail.com>
2025-11-14 12:58:57 -0300

    Fix: Usa python3 en el script de build

--- backend/build.sh
+++ backend/build.sh
@@ -1,16 +1,15 @@
-#!/usr/-bin/env bash
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 # 1. Instala las dependencias
 pip install -r requirements.txt
 
-# 2. Recolecta los archivos estáticos (el CSS/JS del Admin)
-python manage.py collectstatic --no-input
+# 2. Recolecta los archivos estáticos (¡usando python3!)
+python3 manage.py collectstatic --no-input
 
-# 3. Aplica las migraciones de la base de datos
-python manage.py migrate
+# 3. Aplica las migraciones (¡usando python3!)
+python3 manage.py migrate
 
-# 4. Crea el superusuario (¡NUEVA LÍNEA!)
-# Usará las variables de entorno que pusiste en Render
-python manage.py createsuperuser --no-input || echo "Superuser already exists"
+# 4. Crea el superusuario (¡usando python3!)
+python3 manage.py createsuperuser --no-input || echo "Superuser already exists"
